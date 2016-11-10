2d12b1a951ff50e1fe8106175dc256abee781bca
Sharon05 <ssx0405sharpay@163.com>
2016-11-10 19:23:37 +0800

    跟踪目录

--- src/bean/RiskTrackingItemListBean.java
+++ src/bean/RiskTrackingItemListBean.java
@@ -1,5 +1,37 @@
 package bean;
 
-public class RiskTrackingItemListBean {
+import java.io.Serializable;
+import java.util.List;
+
+import model.RiskItem;
+import model.RiskTrackingItem;
+
+public class RiskTrackingItemListBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List riskTrackingItemList;
+
+	
+	public List getRiskTrackingItemList() {
+		return riskTrackingItemList;
+	}
+
+	
+	public void setRiskTrackingItemList(List riskTrackingItemList) {
+		this.riskTrackingItemList = riskTrackingItemList;
+	}
+	
+	
+	public void setriskTrackingItemList(RiskTrackingItem riskTrackingItem, int index) {
+		riskTrackingItemList.set(index, riskTrackingItem);
+	}
+	
+	public RiskTrackingItem getriskTrackingItem(int index) {
+		return (RiskTrackingItem) riskTrackingItemList.get(index);
+	}
 
 }
