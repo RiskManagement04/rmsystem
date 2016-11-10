800c6e4651c949d8b54ed9966687354a882b8f33
Sharon05 <ssx0405sharpay@163.com>
2016-11-10 19:16:53 +0800

    daofactory

--- src/factory/DaoFactory.java
+++ src/factory/DaoFactory.java
@@ -1,8 +1,10 @@
 package factory;
 
 import dao.RiskItemDao;
+import dao.RiskTrackingItemDao;
 import dao.UserDao;
 import daoImp.RiskItemDaoImpl;
+import daoImp.RiskTrackingItemDaoImpl;
 import daoImp.UserDaoImpl;
 
 public class DaoFactory {	
@@ -12,5 +14,9 @@ public class DaoFactory {
 	
 	public static RiskItemDao getRiskItemDao(){
 		return RiskItemDaoImpl.getInstance();
+	}
+	
+	public static RiskTrackingItemDao getRiskTrackingItemDao(){
+		return RiskTrackingItemDaoImpl.getInstance();
 	}
 }
