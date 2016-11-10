4b27f6ef76be2639323a5d13aef90a44ef608f0c
Sharon05 <ssx0405sharpay@163.com>
2016-11-10 19:03:48 +0800

    用户

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -5,5 +5,7 @@ import model.User;
 public interface UserDao {
 	
 	public User findUser(String userId,String password);
+	
+	public boolean addUser();
 
 }
--- src/servlets/RegisterServlet.java
+++ src/servlets/RegisterServlet.java
@@ -1,11 +1,15 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class RegisterServlet
@@ -27,7 +31,14 @@ public class RegisterServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession(false);
+		ServletContext context = getServletContext();
+		response.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
+		PrintWriter pw=response.getWriter();
+		
+		
 	}
 
 	/**
