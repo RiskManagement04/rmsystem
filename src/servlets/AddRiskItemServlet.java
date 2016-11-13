db3b36f691749dee124c1d3f184172d99129819a
Sharon05 <ssx0405sharpay@163.com>
2016-11-13 17:32:55 +0800

    servlet

--- src/servlets/AddRiskItemServlet.java
+++ src/servlets/AddRiskItemServlet.java
@@ -1,11 +1,15 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class AddRiskItemServlet
@@ -27,7 +31,12 @@ public class AddRiskItemServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession(false);
+		ServletContext context = getServletContext();
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("utf-8");
+		PrintWriter pw=response.getWriter();
 	}
 
 	/**
