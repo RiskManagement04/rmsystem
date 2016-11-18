407c5c1838405db14bb4bbdeba060fbce8fb07ce
Sharon05 <ssx0405sharpay@163.com>
2016-11-18 16:27:34 +0800

    风险管理计划bean

---
+++ src/bean/RiskAgendaListBean.java
@@ -0,0 +1,37 @@
+package bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import model.RiskAgenda;
+import model.RiskItem;
+
+public class RiskAgendaListBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List riskAgendaList=null;
+
+	
+	public List getRiskAgendaList() {
+		return riskAgendaList;
+	}
+
+	
+	public void seRiskAgendaList(List riskAgendaList) {
+		this.riskAgendaList = riskAgendaList;
+	}
+	
+	
+	public void setRiskAgendaList(RiskAgenda riskAgenda, int index) {
+		riskAgendaList.set(index, riskAgenda);
+	}
+	
+	public RiskAgenda getRiskAgenda(int index) {
+		return (RiskAgenda) riskAgendaList.get(index);
+	}
+
+}
