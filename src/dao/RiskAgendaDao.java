2d17995a6569689b3a4925d6a45a979b82877802
Sharon05 <ssx0405sharpay@163.com>
2016-11-18 16:16:21 +0800

    风险管理计划接口

--- src/dao/RiskAgendaDao.java
+++ src/dao/RiskAgendaDao.java
@@ -3,11 +3,14 @@ package dao;
 import java.util.List;
 
 import model.RiskAgenda;
+import model.RiskItem;
 
 public interface RiskAgendaDao {
 	
 	public boolean addRiskAgenda(RiskAgenda risk);//新建风险管理计划
 	
-	public List findRiskAgendaByUser(int userId);
+	public List findRiskAgendaByUser(int userId);//获取用户的风险计划列表
+	
+	public boolean addRiskItem(int riskAgendaId,RiskItem riskItem);//在计划中添加风险条目
 
 }
--- src/daoimp/RiskAgendaImpl.java
+++ src/daoimp/RiskAgendaImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import dao.DaoHelper;
 import dao.RiskAgendaDao;
 import model.RiskAgenda;
+import model.RiskItem;
 
 public class RiskAgendaImpl implements RiskAgendaDao{
 	
@@ -29,6 +30,12 @@ public class RiskAgendaImpl implements RiskAgendaDao{
 	public List findRiskAgendaByUser(int userId) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public boolean addRiskItem(int riskAgendaId, RiskItem riskItem) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
