4f9102ad21d98e9eace27e45d743ae05b4c89ccc
Sharon05 <ssx0405sharpay@163.com>
2016-11-18 10:09:39 +0800

    新增风险类别

--- src/model/RiskType.java
+++ src/model/RiskType.java
@@ -3,6 +3,12 @@ package model;
 public enum RiskType {
 	Scope_Risk,//范围风险
 	Schedule_Risk,//进度风险
-	
+	Cost_Risk,//成本风险
+	Quality_Risk,//质量风险
+	Technology_Risk,//技术风险
+	Management_Risk,//管理风险
+	Commercial_Risk,//商业风险
+	Legal_Risk,//法律风险
+	SocialEnvironment_Risk//社会环境风险
 
 }
