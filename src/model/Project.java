a8b60bb781ba0b66e1d5327f370e19ef337f66a1
Sharon05 <ssx0405sharpay@163.com>
2016-11-18 15:19:51 +0800

    项目model

--- src/model/Project.java
+++ src/model/Project.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Project implements Serializable{
 
@@ -13,16 +14,18 @@ public class Project implements Serializable{
 	private String projectName;
 	private int managerId;
 	private String projectContent;
+	private ArrayList<User> users;//项目组成员编号
 	
 	public Project(){
 		
 	}
 	
-	public Project(int projectId,String projectName,int managerId,String projectContent){
+	public Project(int projectId,String projectName,int managerId,String projectContent,ArrayList<User> users){
 		this.projectId=projectId;
 		this.projectName=projectName;
 		this.managerId=managerId;
 		this.projectContent=projectContent;
+		this.setUsers(users);
 	}
 	
 	public int getProjectId() {
@@ -50,6 +53,14 @@ public class Project implements Serializable{
 
 	public void setProjectContent(String projectContent) {
 		this.projectContent = projectContent;
+	}
+
+	public ArrayList<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(ArrayList<User> users) {
+		this.users = users;
 	}
 	
 	
