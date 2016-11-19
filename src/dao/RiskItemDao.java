cf1c65879685eb97566e8a089b56d344d456fdc6
Sharon05 <ssx0405sharpay@163.com>
2016-11-19 11:21:54 +0800

    查找、新增风险跟踪条目

--- src/dao/RiskItemDao.java
+++ src/dao/RiskItemDao.java
@@ -7,6 +7,7 @@ import model.RiskItem;
 public interface RiskItemDao {
 
 	public List findAllRiskItem(int userId);
+	//public List findAllRiskItem();
 	//public boolean addRiskItem(RiskItem riskItem);
 	public boolean deleteRiskItem(int riskItemId);
 }
--- src/daoimp/RiskTrackingItemDaoImpl.java
+++ src/daoimp/RiskTrackingItemDaoImpl.java
@@ -50,6 +50,8 @@ public class RiskTrackingItemDaoImpl implements RiskTrackingItemDao{
 				item.setMeasures(result.getString("r.measures").trim());
 				item.setTrackerName(result.getString("u.trueName").trim());
 				item.setRiskItemName(result.getString("risk.riskName").trim());
+				item.setPossibility(result.getInt("r.possibility"));
+				item.setImpact(result.getInt("r.impact"));
 				
 				trackingItemList.add(item);
 			}
@@ -70,28 +72,60 @@ public class RiskTrackingItemDaoImpl implements RiskTrackingItemDao{
 		java.sql.PreparedStatement statement=null;
 
 		boolean isSuccess=true;
+		//插入风险跟踪条目列表
 		try {
-			statement=con.prepareStatement("insert into RiskTrackingItem(riskItemId,trackerId,createTime,riskStatus,riskContent,measures) values(?,?,?,?,?,?)");
+			statement=con.prepareStatement("insert into RiskTrackingItem(riskItemId,trackerId,createTime,riskStatus,riskContent,measures,"
+					+ "possibility,impact) values(?,?,?,?,?,?,?,?)");
 			statement.setInt(1, item.getRiskItemId());
 			statement.setInt(2,item.getTrackerId());
 			statement.setDate(3, item.getCreateTime());
-			statement.setString(4, item.getRiskStatusString());
-			statement.setString(5,item.getRiskContent());
-			statement.setString(6, item.getMeasures());
+			statement.setString(4, item.getRiskStatusString().trim());
+			statement.setString(5,item.getRiskContent().trim());
+			statement.setString(6, item.getMeasures().trim());
+			statement.setInt(7, item.getPossibility());
+			statement.setInt(8, item.getImpact());
 			
-			statement.execute();
-			
-			
+			statement.execute();						
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			isSuccess=false;
 		}finally{
-			daoHelper.closeConnection(con);
 			daoHelper.closePreparedStatement(statement);			
+		}		
+		
+		if(!isSuccess){//如果插入失败
+			daoHelper.closeConnection(con);
+			return isSuccess;
+		}else{//如果插入成功
+			//根据RiskItemId修改相应的风险条目
+			int i=0;
+			try {
+				statement=con.prepareStatement("update RiskItem set riskContent=?,possibility=?,impact=?,riskStatus=?,measures=?"
+						+ " where riskItemId=?");
+				statement.setString(1, item.getRiskContent().trim());
+				statement.setInt(2, item.getPossibility());
+				statement.setInt(3, item.getImpact());
+				statement.setString(4, item.getRiskStatusString().trim());
+				statement.setString(5, item.getMeasures().trim());
+				statement.setInt(6, item.getRiskItemId());
+				
+				i=statement.executeUpdate();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally{
+				daoHelper.closePreparedStatement(statement);	
+				daoHelper.closeConnection(con);
+			}	
+			
+			if(i!=1){
+				isSuccess=false;
+			}
+			
+			return isSuccess;
 		}
-		
-		
-		return isSuccess;
 	}
 	
 	@Override
@@ -101,7 +135,7 @@ public class RiskTrackingItemDaoImpl implements RiskTrackingItemDao{
 
 		boolean isSuccess=true;
 		try {
-			statement=con.prepareStatement("delete  from RiskTrackingItem where riskItemId=?");
+			statement=con.prepareStatement("delete from RiskTrackingItem where riskItemId=?");
 			statement.setInt(1,riskItemId);
 		
 			
