cd5ad19986c1a5046fb13da985cfa55a62bea8ea
chenzhang <chenzhang@idea-PC>
2016-11-20 17:29:54 +0800

    import

--- src/servlets/ImportRiskServlet.java
+++ src/servlets/ImportRiskServlet.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.RiskItemListBean;
+import model.RiskItem;
 import factory.DaoFactory;
 
 /**
@@ -43,11 +45,15 @@ public class ImportRiskServlet extends HttpServlet {
 		int agendaId=(Integer)session.getAttribute("agendaId");
 		
 		String[] values=request.getParameterValues("isChoosed");
+		
 		ArrayList<Integer> chosedRiskIdLst=new ArrayList<Integer>();
+		if(values.length==0){
+			
+		}
 		for(int i=0;i<values.length;i++){
 			chosedRiskIdLst.add(Integer.parseInt(values[i]));
 		}
-		
+		System.out.println("choose"+chosedRiskIdLst.size());
 		boolean isSuccess=false;
 		try {
 			isSuccess = DaoFactory.getRiskAgendaDao().addRiskItem(agendaId, chosedRiskIdLst);
@@ -55,9 +61,21 @@ public class ImportRiskServlet extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		if(!isSuccess){
 			pw.print("<script>alert('Import failed!');location.href='./login/login.jsp'</script>"); 
 		}else{
+			ArrayList<RiskItem> riskList=new ArrayList<RiskItem>();
+			RiskItemListBean riskItemBean=new RiskItemListBean();
+			
+			try {
+				riskList=DaoFactory.getRiskAgendaDao().findRiskItemByAgenda(agendaId);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			riskItemBean.setRiskItemList(riskList);
+			session.setAttribute("agendaRiskItemList", riskItemBean);
 			 context.getRequestDispatcher("/checkRisk/AgendaRiskItemList.jsp").forward(request, response);
 		}
 		
